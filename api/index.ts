851733dabe06e4ee55d1a4c24b53617b8de4e963
Shiv Shankar Prasad <shivshankarkumar.pusa@gmail.com>
2025-09-02 23:58:01 +0530

    cHANGES

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,10 @@
-import app from '../src/index';
+import createApp from '../src/app.js';
+import { initializeFirebase } from '../src/config/firebase.js';
+
+// Initialize Firebase
+initializeFirebase();
+
+// Create and export the Express app for Vercel
+const app = await createApp();
 
 export default app;
